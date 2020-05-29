19622dfd3857cc01aac65248e93f63ad88932e01
Anastasiia29-11 <56249058+Anastasiia29-11@users.noreply.github.com>
2020-05-29 23:42:20 +0300

    Create books.h

---
+++ books.h
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// структура для хранения полей
+typedef struct book_t {
+    char isbn[16]; 
+    char authors[40];
+    char title[25]; 
+    int total; 
+    int available; 
+} book_t;
+
+book_t read_book(); 
+void print_book(book_t book); 
+
+void print_books(book_t* books, int n); 
+void print_book_by_isbn(book_t* books, int n); 
+
+book_t* add_book(book_t* books, int* n); 
+void remove_book_by_isbn(book_t* books, int* n); 
+
+void save_books(book_t* books, int n); 
+book_t* load_books(int* n); 
